ac4a621885c5581bf83fa25442b96f22096cf8d0
Luka la d <lukas-8@live.fr>
2024-05-30 17:32:14 +0200

    feat: layout landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <main className="flex flex-col w-full text-white">
+      <div className="flex flex-col justify-center items-center min-h-screen w-full bg-gradient-to-b from-[#3E6957] to-[#191919] p-4">
+        <p className="text-4xl lg:text-[70px] leading-tight font-display text-center">
+          VALIDATE <br /> SECURE <br /> EARN
+        </p>
+      </div>
+      <div className="w-full flex flex-col items-center px-80 py-24">
+        <p className="text-[49px]">GNO is Your Key To Staking</p>
+        <div className="w-full flex justify-between mt-8 text-[25px]">
+          <div className="flex flex-col">
+            <p>Total Staked GNO</p>
+          </div>
+          <div className="flex flex-col">
+            <p>Current APY</p>
+          </div>
+          <div className="flex flex-col">
+            <p>Active Validators</p>
+          </div>
+        </div>
+        <p className="text-[49px] mt-24">Choose Your Plan</p>
+        <div className="w-full flex justify-between mt-8 text-[25px]">
+          <div className="flex flex-col">
+            <p>Beginner</p>
+            <p className="text-base">If you do not have the resources to build your own machine or can't afford a machine</p>
+          </div>
+          <div className="flex flex-col">
+            <p>Intermediate</p>
+            <p className="text-base">If you are a hobbyist, can afford a machine but wouldn't mind some GUI</p>
+          </div>
+          <div className="flex flex-col">
+            <p>Advanced</p>
+            <p className="text-base">You are a computer guru, are familiar with interacting with the terminal</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <p className="text-center text-[31px]">
           The best way to gain experience is to be part of the community. Meet the Gnosis Chain validator community. By bringing together Gnosis Chain node operators with our core developers and developers from client teams, we share updates. Join our next event to be a part of the network.
         </p>
-        <div className="w-full flex justify-center gap-x-4 text-[20px] mt-8">
+        <div className="w-full flex justify-center gap-x-4 text-[20px] mt-12">
           <div className=" border rounded-lg p-4 hover:bg-white/15">Events Page</div>
           <div className=" border rounded-lg p-4 hover:bg-white/15">Discord</div>
           <div className=" border rounded-lg p-4 hover:bg-white/15">Validator Newsletter</div>
